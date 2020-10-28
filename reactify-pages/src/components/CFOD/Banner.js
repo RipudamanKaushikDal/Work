6c6872c466d3357f9fa8b59aea37959a9454269e
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2020-10-28 16:31:41 -0300

    Added swipeable banner to the page

--- reactify-pages/src/components/CFOD/Banner.js
+++ reactify-pages/src/components/CFOD/Banner.js
@@ -1,43 +1,44 @@
 // Import necessary modules
 import React from 'react'
 import './Banner.css'
-
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
 
 function Banner({data}) {
 
-    // Code to choose a random data object from the dummy Static Data everytime it loads
-    
-    // Create a random integer between 1 and length of data array
-    let randomInt = Math.floor(
-        Math.random(0,1) * (data.length - 1)
-      );
+  
 
-      // Chosen data from the Static data
-    const selectedData = data[randomInt]
+    // Library to add auto-carousel effect to banner
+
+    const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 
     return (
 
-        // Render various banner components
-        <>
-        <header className="banner" style={{backgroundImage:`url(${selectedData.image})`}}>
-            <div className="banner_contents">
-                <h1 className="banner_title">
-                    {selectedData.title}
-                </h1>
+        // Render various banner components with a carousel effect
+        <AutoPlaySwipeableViews interval={4000} >
+        {data.map(selectedData => (
+                <header className="banner" style={{backgroundImage:`url(${selectedData.image})`}} key={selectedData.title}>
+                    <div className="banner_contents">
+                        <h1 className="banner_title">
+                            {selectedData.title}
+                        </h1>
 
-                <div className="banner_description">
-                    <h4>{selectedData.description}</h4>
-                </div>
+                        <div className="banner_description">
+                            <h4>{selectedData.description}</h4>
+                        </div>
 
-                <div >
-                    <button className="banner_button" >Start Now</button>
-                </div>                
-            </div>
-            <div className="banner--fadebottom"></div>
-        </header> 
+                        <div >
+                            <button className="banner_button" >Start Now</button>
+                        </div>                
+                    </div>
+                    <div className="banner--fadebottom"></div>
+            </header> 
         
-        </>
+
+        ))}
+        
+    </AutoPlaySwipeableViews>
     )
 }
 
